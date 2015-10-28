ffa8eff99a820b0e4e566c4fb0ee15a1e196caa7
bkillenit <blaine.j.killen@gmail.com>
2015-10-28 15:54:38 -0700

    added nodemon and setup stub handling for POST to index route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res, next) {
+  var partialSearchTerm = req.body.searchterm;
+
+  console.log('yay');
+
+  res.send('nice');
 });
 
 module.exports = router;
